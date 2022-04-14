7b3fc1f8850ea237606a555391285b9a9cdef8f4
Celtica3 <celticaxp@gmail.com>
2022-04-14 16:20:26 -0700

    Update honkai_tracker.py
    add lookup, update

--- bot_cogs/honkai_tracker.py
+++ bot_cogs/honkai_tracker.py
@@ -1,9 +1,13 @@
+
 from discord.ext import commands
 from discord.ext.commands import Bot
+from alib.tracker import HonkaiMember
 from bot.conf import bot_bridge
 import discord
 from discord.interactions import InteractionResponse
 from bot_ui.reg import uid_form
+from discord.utils import get
+from alib.dbot import has_roles
 
 class cog_tracker(commands.Cog):
     def __init__(self, bot):
@@ -21,16 +25,73 @@ class cog_tracker(commands.Cog):
         form = uid_form()
 
         await ires.send_modal(form)
-        bot_bridge._honkai_tracker.save()
-    
+        
     @commands.slash_command(name="unbind", guild_ids=bot_bridge.allowed_servers)
     @commands.has_any_role("Impact Vice Leader", "Impact Leader")
     async def unbind(self, ctx, user : discord.User):
-        bot_bridge._honkai_tracker.remove_member_by_attr("discord_id", user.id)
+        if bot_bridge._honkai_tracker.remove_member_by_attr("discord_id", user.id):
+            bot_bridge._honkai_tracker.save()
         embed = discord.Embed(title="User Unbinded", description="Unbinded {}".format(user.mention))
         ires : InteractionResponse = ctx.interaction.response
         await ires.send_message(embed=embed)
+        
+
+    @commands.slash_command(name="lookup", guild_ids=bot_bridge.allowed_servers)
+    async def lookup(self, ctx : discord.ApplicationContext, user : discord.User = None, uid : int = None):
+        ires : InteractionResponse = ctx.interaction.response
+        if user is None and uid is None:
+            user = ctx.author
+        
+        if user is not None:
+            member : HonkaiMember = bot_bridge._honkai_tracker.get_member(discord_id=user.id)
+        
+            if member is None:
+                embed = discord.Embed(title="Error", description="User not registered")
+                return await ires.send_message(embed=embed)
+
+            username = user.name
+        else:
+            member : HonkaiMember = bot_bridge._honkai_tracker.get_member(uid=uid)
+
+            if member is None:
+                embed = discord.Embed(title="Error", description="User not found")
+                return await ires.send_message(embed=embed)
+
+            username = member.uid
+            if get(ctx.guild.members, id=member.discord_id) is not None:
+                username = get(ctx.guild.members, id=member.discord_id).name
+            
+        embed = discord.Embed(title=f"{username} Lookup", description=f"by {ctx.author.mention}")
+        
+        embed.add_field(name="uid", value=member.uid)
+        embed.add_field(name="lv", value=member.lv)
+        
+        return await ires.send_message(embed=embed)
+
+    @commands.slash_command(name="update", guild_ids=bot_bridge.allowed_servers)
+    async def updateinfo(self, ctx : discord.ApplicationContext, lv : int = None, other_user : discord.User = None):
+        ires : InteractionResponse = ctx.interaction.response
+        if other_user is None:
+            user = ctx.author
+        else:
+            user = other_user
+        
+        # check permissions
+        if other_user and not has_roles(ctx, *bot_bridge.MOD_ROLES):
+            embed = discord.Embed(title="Error", description="You don't have permission to update other users")
+            return await ires.send_message(embed=embed)
+
+        # check if user is registered
+        member : HonkaiMember = bot_bridge._honkai_tracker.get_member(discord_id=user.id)
+        if member is None:
+            embed = discord.Embed(title="Error", description="User not registered")
+            return await ires.send_message(embed=embed)
+        
+        member.lv = member.lv if lv is None else lv
         bot_bridge._honkai_tracker.save()
+        embed = discord.Embed(title="User Updated", description="Updated {}".format(user.mention))
+        return await ires.send_message(embed=embed)
+    
 
 def setup(bot):
     bot.add_cog(cog_tracker(bot))
