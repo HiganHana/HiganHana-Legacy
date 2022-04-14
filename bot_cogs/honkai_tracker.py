4ce2c07af2c60625265bf578a59d9db888c5c70d
Celtica3 <celticaxp@gmail.com>
2022-04-14 14:54:31 -0700

    add unbind command

--- bot_cogs/honkai_tracker.py
+++ bot_cogs/honkai_tracker.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from bot_ui.reg import ask_in_game_uid
 from bot.conf import bot_bridge
+import discord
 
 class tester(commands.Cog):
     def __init__(self, bot):
@@ -10,7 +11,13 @@ class tester(commands.Cog):
     @commands.slash_command(name="register", guild_ids=bot_bridge.allowed_servers)
     async def register(self, ctx):
         await ask_in_game_uid(ctx)
-        
+    
+    @commands.slash_command(name="unbind", guild_ids=bot_bridge.allowed_servers)
+    async def unbind(self, ctx, user : discord.User):
+        bot_bridge._honkai_tracker.remove_member_by_attr("discord_id", user.id)
+        embed = discord.Embed(title="User Unbinded", description="Unbinded {}".format(user.mention))
+        await ctx.send(embed=embed)
+        bot_bridge._honkai_tracker.save()
 
 def setup(bot):
     bot.add_cog(tester(bot))
