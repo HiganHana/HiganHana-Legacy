c7aeae61104c932825dd0b31fedd6ccbfa07930e
Celtica3 <celticaxp@gmail.com>
2022-04-14 15:26:22 -0700

    Update debug.py

--- bot_cogs/debug.py
+++ bot_cogs/debug.py
@@ -13,7 +13,9 @@ class cog_debug(commands.Cog):
     @commands.command(name="dump_tracker")
     @commands.has_guild_permissions(administrator=True)
     async def dump_tracker(self, ctx):
-        await ctx.send(f'"""{pformat(bot_bridge._honkai_tracker.__real_data__)}"""')
+        await ctx.send(f"""```
+        {pformat(bot_bridge._honkai_tracker.__real_data__)}
+    ```""")
 
 def setup(bot):
     bot.add_cog(cog_debug(bot))
