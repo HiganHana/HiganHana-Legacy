e99bc91c03e5689863c83bae4a5426792534516f
Celtica3 <celticaxp@gmail.com>
2022-04-14 14:56:18 -0700

    remove register function in new_member

--- bot_cogs/new_member.py
+++ bot_cogs/new_member.py
@@ -38,11 +38,7 @@ class new_member(commands.Cog):
         )
         await ctx.respond(embed = embed)
     
-    #Honkai player register
-    @commands.slash_command(name="register", guild_ids=bot_bridge.allowed_servers)
-    async def register(self, ctx):
-        await ask_in_game_uid(ctx)
-        bot_bridge._honkai_tracker.save()
+ 
 
 def setup(bot):
     bot.add_cog(new_member(bot))
