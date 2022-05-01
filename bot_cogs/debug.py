f74385177455707876f2d961d83765d795b6bdab
Celtica3 <celticaxp@gmail.com>
2022-05-01 02:22:26 -0700

    feat: debug++

--- bot_cogs/debug.py
+++ bot_cogs/debug.py
@@ -1,5 +1,6 @@
 from contextlib import redirect_stdout
 from pprint import pformat
+import typing
 from discord.ext import commands
 from discord.ext.commands import Bot
 from bot.conf import bot_bridge
@@ -9,6 +10,7 @@ from bot_ui.reg import uid_form
 import honkaiDex
 from honkaiDex import Battlesuit,BaseCharacter,StigamataSet
 import io
+import inspect
 class cog_debug(commands.Cog):
     def __init__(self, bot):
         self.bot : Bot = bot
@@ -44,23 +46,50 @@ class cog_debug(commands.Cog):
         
         code = str(line).strip()
         embed = discord.Embed(title="Eval Result", description=f"input:```{code}```")
+
+        if "os.system" in code:
+            embed.add_field(name="Error", value="os.system is not allowed")
+            return await ctx.respond(embed=embed)
+
         try:
             result = eval(code)
 
             output = None
             with io.StringIO() as buf, redirect_stdout(buf):
-                if callable(result):
-                    result = result()
-                    output = buf.getvalue()
+                result = await self.exec_eval(result)
+                output = buf.getvalue()
                     
-                    if output is not None:
-                        embed.add_field(name="stdout", value=f"```{output}```")
+                if output is not None:
+                    embed.add_field(name="stdout", value=f"```{output}```")
             embed.add_field(name="result", value=f"```{pformat(result)}```")
             
             
         except Exception as e:
             embed.add_field(name="error", value=f"```{pformat(e)}```")
         await ctx.send(embed=embed)
+
+    async def exec_callable(self, val, *args):
+        if inspect.iscoroutinefunction(val):
+            return await val(*args)
+
+        val = val(*args)
+        return val
+        
+
+    async def exec_eval(self, val):
+        if callable(val):
+            return await self.exec_callable(val)
+
+        if isinstance(val, typing.Iterable) and len(val) == 1:
+            val = val[0]
+            return await self.exec_callable(val)
+
+        if isinstance(val, typing.Iterable) and len(val) > 1:
+            val, *args = val
+
+            return await self.exec_callable(val, *args)
+        
+        return val
 
 def setup(bot):
     bot.add_cog(cog_debug(bot))
